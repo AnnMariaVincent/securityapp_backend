7b9d1354b84d7fcf8a7284cc608b35355b05c830
AnnMariaVincet <annmariavincent05@gmail.com>
2024-02-13 22:15:23 +0530

    addsecurity

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const cors=require("cors")
 const mongoose=require("mongoose")
-const adminrouter=require("./controller/addsecurityrouter")
+const adminRouter=require("./controller/addsecurityrouter")
 
 const app=express()
 
@@ -11,7 +11,7 @@ app.use(cors())
 mongoose.connect("mongodb+srv://annvinc:annvinc@cluster0.ovg3s8x.mongodb.net/securityDb?retryWrites=true&w=majority")
 
 
-app.use("/api/security",adminrouter)
+app.use("/api/security",adminRouter)
 
 app.listen(3001,()=>{
     console.log("server running")
--- model/adminmodel.js
+++ model/adminmodel.js
@@ -2,7 +2,13 @@ const mongoose=require("mongoose")
 
 const addsecuritySchema=new mongoose.Schema(
     {
-       
+       name:String,
+       empid:String,
+       address:String,
+       phno:String,
+       emailid:String,
+       pass:String,
+    
     }
 )
 
